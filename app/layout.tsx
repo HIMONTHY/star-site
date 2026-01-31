52a78077840571849c000d83edd57894d72fc1b4
HIMONTHY <Auto3srobloxgoat@gmail.com>
2026-01-31 04:10:16 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata = {
     siteName: "Star Site",
     images: [
       {
-        url: "https://discord.com/channels/@me/1117716359267225611/1467083237716131852",
+        url: "https://media.discordapp.net/attachments/1117716359267225611/1467083237816930386/ChatGPT_Image_Jan_31_2026_03_59_20_AM.png?ex=697f173c&is=697dc5bc&hm=575ebf5cd704f3e5bb558cfbf76d8dd11a88eb98115bbe1655290d1fde01fa51&=&format=webp&quality=lossless&width=1310&height=873",
         width: 1200,
         height: 630,
       },
